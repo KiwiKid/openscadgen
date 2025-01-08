1071563c021a7af60249627704bd8e455b0b1def
KiwiKid <5018297+KiwiKid@users.noreply.github.com>
2025-01-09 09:27:02 +1300

    fix go release/3

--- main.go
+++ main.go
@@ -358,7 +358,7 @@ func getOutputPath(config Config) string {
 const OPENSCAD_VERSION_WARN_IF_OLDER_THAN = 2024
 
 func main() {
-	VERSION := "v0.9.5-alpha"
+	VERSION := "v0.9.7-alpha"
 
 	startTime := time.Now()
 
