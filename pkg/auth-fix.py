96d65814f36c92f3eabf3bf2b238b9a8e8b93a33
KiwiKid <5018297+KiwiKid@users.noreply.github.com>
2025-03-16 23:55:38 +1300

    tweak naming -  v2.0.11-BETA

--- pkg/auth-fix.py
+++
@@ -1,6 +0,0 @@
-def name_callback(name, email):
-    if not name or not email:
-        return "Unknown", "unknown@example.com"
-    if name == "greg-cumming-le":
-        return "KiwiKid", "5018297+KiwiKid@users.noreply.github.com"
-    return name, email
