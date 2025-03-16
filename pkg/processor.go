d303fdc6d6a21a697eed84b6b493850cbfff0fd2
KiwiKid <5018297+KiwiKid@users.noreply.github.com>
2025-03-16 20:25:07 +1300

    bump version

--- pkg/processor.go
+++ pkg/processor.go
@@ -215,7 +215,7 @@ To create a new version:
 git commit -m "New and improved version"
 git tag "v[NEW_VERSION_HERE]-alpha"
 */
-const VERSION = "v2.0.1-BETA"
+const VERSION = "v2.0.2-BETA"
 
 func Process(cmdFlags CmdFlags) {
 
