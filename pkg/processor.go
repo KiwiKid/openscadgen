26face9566312bdeaf1a4f216b53b601af5e7270
KiwiKid <5018297+KiwiKid@users.noreply.github.com>
2025-05-03 13:35:28 +1200

    re-publish version

--- pkg/processor.go
+++ pkg/processor.go
@@ -136,7 +136,7 @@ To create a new version:
 git commit -m "New and improved version"
 git tag "v[NEW_VERSION_HERE]-alpha"
 */
-const VERSION = "v2.3.1-BETA"
+const VERSION = "v2.3.2-BETA"
 
 type Version struct {
 	OpenSCADGen string
