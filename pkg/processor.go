502515a02fc392ebed053f08851686422684ccac
KiwiKid <5018297+KiwiKid@users.noreply.github.com>
2025-08-02 18:01:20 +1200

    Improve loading bars and terminal output

--- pkg/processor.go
+++ pkg/processor.go
@@ -145,7 +145,7 @@ To create a new version:
 git commit -m "New and improved version"
 git tag "v[NEW_VERSION_HERE]-alpha"
 */
-const VERSION = "v2.7.13"
+const VERSION = "v2.7.14"
 
 type Version struct {
 	OpenSCADGen string
