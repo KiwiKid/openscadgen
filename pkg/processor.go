c175d8732f5ac7bd9ddd721043e119f96b32bcb8
KiwiKid <5018297+KiwiKid@users.noreply.github.com>
2026-01-18 14:49:51 +1300

    refine server mode navigation handling /2

--- pkg/processor.go
+++ pkg/processor.go
@@ -146,7 +146,7 @@ To create a new version:
 git commit -m "New and improved version"
 git tag "v[NEW_VERSION_HERE]-alpha"
 */
-const VERSION = "v2.7.30"
+const VERSION = "v2.7.31"
 
 type Version struct {
 	OpenSCADGen string
